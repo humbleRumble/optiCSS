c3e0999c7f4b8b3ae96039119bffdf3e6d113028
humbleRumble <ajsa@fiveblueheron.com>
2015-05-29 22:50:23 -0600

    Update optiCSS.js

--- optiCSS.js
+++ optiCSS.js
@@ -1,6 +1,7 @@
 var stylesheets = document.styleSheets, stylesheet, i;
 for (i = 0; (stylesheet = stylesheets[i]); i++) {
     var rules = stylesheet.rules, rule, j;
+    if(!rules.length) continue;
     var newRules = [];
     for (j = 0; (rule = rules[j]); j++) {
         if(rule.type === rule.STYLE_RULE) {
@@ -9,13 +10,12 @@ for (i = 0; (stylesheet = stylesheets[i]); i++) {
                 for(k = 0; (selector = selectors[k]); k++) {
                     var styles = rule.style, style, l;
                     var elements = document.querySelectorAll(selector.trim()), element, l;
-                    if(elements.length) {
-                        var styleString = '';
-                        for(m = 0; (style = styles[m]); m++) {
-                            styleString += style + ': ' + styles.getPropertyValue(style) + '; ';
-                        }
-                        newRules.push((selector.trim() + ' { ' + styleString.trim() + ' }'));
+                    if(!elements.length) continue;
+                    var styleString = '';
+                    for(m = 0; (style = styles[m]); m++) {
+                        styleString += style + ': ' + styles.getPropertyValue(style) + '; ';
                     }
+                    newRules.push((selector.trim() + ' { ' + styleString.trim() + ' }'));
                 }
                 stylesheet.deleteRule(j);
             }
@@ -32,31 +32,31 @@ for (i = 0; (stylesheet = stylesheets[i]); i++) {
         if(rule.type === rule.STYLE_RULE) {
             var styles = rule.style, style, k;
             var elements = document.querySelectorAll(rule.selectorText);
-            if(elements.length) {
-                for(k = 0; (style = styles[k]); k++) {
-                    var value = styles.getPropertyValue(style);
-                    if(value.indexOf('%') < 0) {
-                        var elements = document.querySelectorAll(rule.selectorText), element, m;
-                        var keep = false;
-                        for(m = 0; (element = elements[m]); m++) {
-                            var computed = window.getComputedStyle(element).getPropertyValue(style);
-                            var match1 = value === computed;
-                            styles.removeProperty(style);
-                            var computed = window.getComputedStyle(element).getPropertyValue(style);
-                            var match2 = value === computed;
-                            styles.setProperty(style, value);
-                            if( match1 && !match2 ) {
-                                keep = true;
-                            }
-                        }
-                        if(!keep) {
-                            styles.removeProperty(style);
+            if(!elements.length) continue;
+            for(k = 0; (style = styles[k]); k++) {
+                var value = styles.getPropertyValue(style);
+                if(value.indexOf('%') < 0) {
+                    var keep = false;
+                    var elements = document.querySelectorAll(rule.selectorText), element, m;
+                    if(!elements.length) continue;
+                    for(m = 0; (element = elements[m]); m++) {
+                        var computed = window.getComputedStyle(element).getPropertyValue(style);
+                        var match1 = value === computed;
+                        styles.removeProperty(style);
+                        var computed = window.getComputedStyle(element).getPropertyValue(style);
+                        var match2 = value === computed;
+                        styles.setProperty(style, value);
+                        if( match1 && !match2 ) {
+                            keep = true;
                         }
                     }
+                    if(!keep) {
+                        styles.removeProperty(style);
+                    }
                 }
-                if (styles.length) {
-                   ruleText += rule.cssText + "\n";
-                }
+            }
+            if (styles.length) {
+                ruleText += rule.cssText + "\n";
             }
         } else {
             ruleText += rule.cssText + "\n";
